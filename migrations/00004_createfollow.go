973af7c3460af5ba13d5c52f3033e556dc04c198
Morumpalli Vinay Kumar <vinay.kumar@joshtechnologygroup.com>
2024-09-13 13:00:56 +0530

    added follow models and created migration

---
+++ migrations/00004_createfollow.go
@@ -0,0 +1,35 @@
+package migrations
+
+import (
+	"app/models"
+	"context"
+	"database/sql"
+
+	"github.com/pressly/goose/v3"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func init() {
+	goose.AddMigrationContext(upCreatefollow, downCreatefollow)
+}
+
+func upCreatefollow(ctx context.Context, tx *sql.Tx) error {
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		Conn: tx,
+	}), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+	return db.Migrator().CreateTable(&models.Follow{})
+}
+
+func downCreatefollow(ctx context.Context, tx *sql.Tx) error {
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		Conn: tx,
+	}), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+	return db.Migrator().CreateTable(&models.Follow{})
+}
--- models/follow.go
+++ models/follow.go
@@ -1,6 +1,10 @@
 package models
 
 type Follow struct {
-	Follower  uint
-	Following uint
+	ID        uint `gorm:"primaryKey"`
+	Follower  uint `gorm:"not null"`
+	Following uint `gorm:"not null"`
+
+	FollowerUser  UserDetails `gorm:"foreignkey:Follower;references:ID;constraint:OnDelete:CASCADE;"`
+	FollowingUser UserDetails `gorm:"foreignkey:Following;references:ID;constraint:OnDelete:CASCADE;"`
 }
